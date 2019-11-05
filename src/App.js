cd2d4ae9a503a4bc97bc3d24f871ff0ad69dbe95
qardpeet <patplaysg@gmail.com>
2019-11-05 21:57:50 +0400

    Created starting markup for the game -Made sure everything is responsive

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import React, { Component } from 'react';
 export class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Test</h1>
-      </div>
+      <>
+        <div className="menu">
+          <div className="menu-content">
+            <h3>
+              SCORE: <span className="accent">3</span>
+            </h3>
+          </div>
+        </div>
+        <div className="city-cards-container">
+          <div className="city-card">
+            <h2>Tbilisi</h2>
+            <h2>Georgia</h2>
+          </div>
+          <div className="middle">
+            <h3>OR</h3>
+          </div>
+          <div className="city-card">
+            <h2>Tbilisi</h2>
+            <h2>Georgia</h2>
+            <span className="temperature">+15 C</span>
+          </div>
+        </div>
+      </>
     );
   }
 }
