f3ffe7ef0c12124c882e9bf55aed381818312ead
qardpeet <patplaysg@gmail.com>
2019-11-07 20:22:03 +0400

    Updates

---
+++ precache-manifest.ed3a272b3685ef5786567e8b60fa3333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004cb23ae4ce4f0065ce347e37baaf64",
+    "url": "/pixelart-interview-project/index.html"
+  },
+  {
+    "revision": "25e65a172e4a0247f408",
+    "url": "/pixelart-interview-project/static/css/main.faf49f6d.chunk.css"
+  },
+  {
+    "revision": "2c450cceb381f0d174a9",
+    "url": "/pixelart-interview-project/static/js/2.6428e84a.chunk.js"
+  },
+  {
+    "revision": "25e65a172e4a0247f408",
+    "url": "/pixelart-interview-project/static/js/main.44032172.chunk.js"
+  },
+  {
+    "revision": "a70f14418d58a6b4e969",
+    "url": "/pixelart-interview-project/static/js/runtime-main.aa6aa211.js"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/pixelart-interview-project/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/pixelart-interview-project/static/media/Poppins-Regular.41e8dead.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed2b913fbfdfe656bad907a002522677.js"
+  "/pixelart-interview-project/precache-manifest.ed3a272b3685ef5786567e8b60fa3333.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pixelart-interview-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
